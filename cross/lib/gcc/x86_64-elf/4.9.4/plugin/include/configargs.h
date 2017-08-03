cfca651d43d2e4ea6b6389c52789ae80cd3f0840
tastymeatloaf <flinchindunsparce@gmail.com>
2017-08-03 08:58:26 -0500

    initial commit -- gcc 4.9.4 + binutils 2.27 cross compiler only

---
+++ cross/lib/gcc/x86_64-elf/4.9.4/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.4//configure --target=x86_64-elf --prefix=/home/kyle/opt/cross --disable-nls --enable-languages=c,c++ --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
